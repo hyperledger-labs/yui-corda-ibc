13bd4382701244d81676810b5d7ea2d4f18c7789
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2020-04-30 11:50:10 +0900

    Implement flows for connOpen{Try, Ack, Confirm}
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

---
+++ workflows/src/main/kotlin/jp/datachain/corda/ibc/flows/IbcConnOpenAckFlow.kt
@@ -0,0 +1,84 @@
+package jp.datachain.corda.ibc.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import jp.datachain.corda.ibc.contracts.Ibc
+import jp.datachain.corda.ibc.ics2.ClientState
+import jp.datachain.corda.ibc.ics23.CommitmentProof
+import jp.datachain.corda.ibc.ics24.Host
+import jp.datachain.corda.ibc.ics24.Identifier
+import jp.datachain.corda.ibc.ics25.Handler.connOpenAck
+import jp.datachain.corda.ibc.states.Connection
+import jp.datachain.corda.ibc.types.Height
+import jp.datachain.corda.ibc.types.Version
+import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+object IbcConnOpenAckFlow {
+    @StartableByRPC
+    @InitiatingFlow
+    class Initiator(
+            val identifier: Identifier,
+            val version: Version.Single,
+            val proofTry: CommitmentProof,
+            val proofConsensus: CommitmentProof,
+            val proofHeight: Height,
+            val consensusHeight: Height
+    ) : FlowLogic<SignedTransaction>() {
+        @Suspendable
+        override fun call() : SignedTransaction {
+            val notary = serviceHub.networkMapCache.notaryIdentities.single()
+
+            val builder = TransactionBuilder(notary)
+
+            val host = serviceHub.vaultService.queryBy<Host>().states.single() // assuming there's only one host
+            val participants = host.state.data.participants.map{it as Party}
+            require(participants.contains(ourIdentity))
+
+            val conn = serviceHub.vaultService.queryBy<Connection>(
+                    QueryCriteria.LinearStateQueryCriteria(participants, listOf(identifier.toUniqueIdentifier()))).states.single()
+
+            val client = serviceHub.vaultService.queryBy<ClientState>(
+                    QueryCriteria.LinearStateQueryCriteria(participants, listOf(conn.state.data.end.clientIdentifier.toUniqueIdentifier()))).states.single()
+
+            val newConn = Triple(host.state.data, client.state.data, conn.state.data).connOpenAck(
+                    identifier,
+                    version,
+                    proofTry,
+                    proofConsensus,
+                    proofHeight,
+                    consensusHeight)
+
+            builder.addCommand(Ibc.Commands.ConnOpenAck(
+                    identifier,
+                    version,
+                    proofTry,
+                    proofConsensus,
+                    proofHeight,
+                    consensusHeight
+            ), ourIdentity.owningKey)
+                    .addInputState(host)
+                    .addInputState(client)
+                    .addInputState(conn)
+                    .addOutputState(newConn)
+
+            val tx = serviceHub.signInitialTransaction(builder)
+
+            val sessions = (participants - ourIdentity).map{initiateFlow(it)}
+            val stx = subFlow(FinalityFlow(tx, sessions))
+            return stx
+        }
+    }
+
+    @InitiatedBy(Initiator::class)
+    class Responder(val counterPartySession: FlowSession) : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call() {
+            val stx = subFlow(ReceiveFinalityFlow(counterPartySession))
+            println(stx)
+        }
+    }
+}
---
+++ workflows/src/main/kotlin/jp/datachain/corda/ibc/flows/IbcConnOpenConfirmFlow.kt
@@ -0,0 +1,74 @@
+package jp.datachain.corda.ibc.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import jp.datachain.corda.ibc.contracts.Ibc
+import jp.datachain.corda.ibc.ics2.ClientState
+import jp.datachain.corda.ibc.ics23.CommitmentProof
+import jp.datachain.corda.ibc.ics24.Host
+import jp.datachain.corda.ibc.ics24.Identifier
+import jp.datachain.corda.ibc.ics25.Handler.connOpenConfirm
+import jp.datachain.corda.ibc.states.Connection
+import jp.datachain.corda.ibc.types.Height
+import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+object IbcConnOpenConfirmFlow {
+    @StartableByRPC
+    @InitiatingFlow
+    class Initiator(
+            val identifier: Identifier,
+            val proofAck: CommitmentProof,
+            val proofHeight: Height
+    ) : FlowLogic<SignedTransaction>() {
+        @Suspendable
+        override fun call() : SignedTransaction {
+            val notary = serviceHub.networkMapCache.notaryIdentities.single()
+
+            val builder = TransactionBuilder(notary)
+
+            val host = serviceHub.vaultService.queryBy<Host>().states.single() // assuming there's only one host
+            val participants = host.state.data.participants.map{it as Party}
+            require(participants.contains(ourIdentity))
+
+            val conn = serviceHub.vaultService.queryBy<Connection>(
+                    QueryCriteria.LinearStateQueryCriteria(participants, listOf(identifier.toUniqueIdentifier()))).states.single()
+
+            val client = serviceHub.vaultService.queryBy<ClientState>(
+                    QueryCriteria.LinearStateQueryCriteria(participants, listOf(conn.state.data.end.clientIdentifier.toUniqueIdentifier()))).states.single()
+
+            val newConn = Triple(host.state.data, client.state.data, conn.state.data).connOpenConfirm(
+                    identifier,
+                    proofAck,
+                    proofHeight)
+
+            builder.addCommand(Ibc.Commands.ConnOpenConfirm(
+                    identifier,
+                    proofAck,
+                    proofHeight
+            ), ourIdentity.owningKey)
+                    .addInputState(host)
+                    .addInputState(client)
+                    .addInputState(conn)
+                    .addOutputState(newConn)
+
+            val tx = serviceHub.signInitialTransaction(builder)
+
+            val sessions = (participants - ourIdentity).map{initiateFlow(it)}
+            val stx = subFlow(FinalityFlow(tx, sessions))
+            return stx
+        }
+    }
+
+    @InitiatedBy(Initiator::class)
+    class Responder(val counterPartySession: FlowSession) : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call() {
+            val stx = subFlow(ReceiveFinalityFlow(counterPartySession))
+            println(stx)
+        }
+    }
+}
