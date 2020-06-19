2dfb5cd91ae48edd30248601afbb4a7a4cb4ab83
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2020-06-19 21:44:33 +0900

    cosmos-experiment: add ChanOpenAck & ChanOpenConfirm & ChanCloseInit & ChanCloseConfirm experiments

---
+++ cosmos-experiment/src/main/kotlin/jp/datachain/cosmos/x/ibc/ics04_channel/client/rest/ChannelOpenConfirmReq.kt
@@ -0,0 +1,17 @@
+package jp.datachain.cosmos.x.ibc.ics04_channel.client.rest
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import jp.datachain.corda.ibc.cosmos.CosmosRequest
+import jp.datachain.corda.ibc.cosmos.ReqPath
+import jp.datachain.cosmos.types.rest.BaseReq
+import jp.datachain.cosmos.x.ibc.ics23_commitment.types.MerkleProof
+
+@ReqPath("ibc/ports/%s/channels/%s/open-confirm")
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
+data class ChannelOpenConfirmReq(
+        val proofAck: MerkleProof,
+        val proofHeight: String /*uint64*/
+) : CosmosRequest {
+        override var baseReq: BaseReq? = null
+}
