75caeb3f8c49ab097ccdcd1be2c34e2550b02814
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2020-11-06 17:13:51 +0900

    ICS-25,26: refactor Handler::connOpenConfirm using Context and introduce HandleConnOpenConfirm
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

---
+++ contracts/src/main/kotlin/jp/datachain/corda/ibc/ics26/HandleConnOpenConfirm.kt
@@ -0,0 +1,21 @@
+package jp.datachain.corda.ibc.ics26
+
+import jp.datachain.corda.ibc.ics23.CommitmentProof
+import jp.datachain.corda.ibc.ics24.Identifier
+import jp.datachain.corda.ibc.ics25.Handler
+import jp.datachain.corda.ibc.types.Height
+import java.security.PublicKey
+
+data class HandleConnOpenConfirm(
+        val identifier: Identifier,
+        val proofAck: CommitmentProof,
+        val proofHeight: Height
+): DatagramHandler {
+    override fun execute(ctx: Context, signers: Collection<PublicKey>) {
+        Handler.connOpenConfirm(
+                ctx,
+                identifier,
+                proofAck,
+                proofHeight)
+    }
+}
