7ddac30543034ada423627a6b712fdae26cf33a9
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-01-21 16:32:34 +0900

    grpc: add implementations of packetCommitment/packetReceipt/packetAcknowledgement methods of ChannelQueryService

--- grpc-adapter/src/main/kotlin/jp/datachain/corda/ibc/grpc_adapter/ChannelQueryService.kt
+++ grpc-adapter/src/main/kotlin/jp/datachain/corda/ibc/grpc_adapter/ChannelQueryService.kt
@@ -27,4 +27,52 @@ class ChannelQueryService(host: String, port: Int, username: String, password: S
         responseObserver.onNext(reply)
         responseObserver.onCompleted()
     }
+
+    override fun packetCommitment(request: QueryOuterClass.QueryPacketCommitmentRequest, responseObserver: StreamObserver<QueryOuterClass.QueryPacketCommitmentResponse>) {
+        val stateAndRef = ops.vaultQueryBy<IbcChannel>(QueryCriteria.LinearStateQueryCriteria(
+                externalId = listOf(baseId.toString()),
+                uuid = listOf(Identifier(request.channelId).toUUID())
+        )).states.single()
+        assert(stateAndRef.state.data.portId.id == request.portId)
+        val proof = ops.internalFindVerifiedTransaction(stateAndRef.ref.txhash)!!.toProof()
+        val reply = QueryOuterClass.QueryPacketCommitmentResponse.newBuilder()
+                .setCommitment(stateAndRef.state.data.packets[request.sequence]!!.toByteString())
+                .setProof(proof.toByteString())
+                .setProofHeight(Client.Height.getDefaultInstance())
+                .build()
+        responseObserver.onNext(reply)
+        responseObserver.onCompleted()
+    }
+
+    override fun packetReceipt(request: QueryOuterClass.QueryPacketReceiptRequest, responseObserver: StreamObserver<QueryOuterClass.QueryPacketReceiptResponse>) {
+        val stateAndRef = ops.vaultQueryBy<IbcChannel>(QueryCriteria.LinearStateQueryCriteria(
+                externalId = listOf(baseId.toString()),
+                uuid = listOf(Identifier(request.channelId).toUUID())
+        )).states.single()
+        assert(stateAndRef.state.data.portId.id == request.portId)
+        val proof = ops.internalFindVerifiedTransaction(stateAndRef.ref.txhash)!!.toProof()
+        val reply = QueryOuterClass.QueryPacketReceiptResponse.newBuilder()
+                .setReceived(stateAndRef.state.data.receipts.contains(request.sequence))
+                .setProof(proof.toByteString())
+                .setProofHeight(Client.Height.getDefaultInstance())
+                .build()
+        responseObserver.onNext(reply)
+        responseObserver.onCompleted()
+    }
+
+    override fun packetAcknowledgement(request: QueryOuterClass.QueryPacketAcknowledgementRequest, responseObserver: StreamObserver<QueryOuterClass.QueryPacketAcknowledgementResponse>) {
+        val stateAndRef = ops.vaultQueryBy<IbcChannel>(QueryCriteria.LinearStateQueryCriteria(
+                externalId = listOf(baseId.toString()),
+                uuid = listOf(Identifier(request.channelId).toUUID())
+        )).states.single()
+        assert(stateAndRef.state.data.portId.id == request.portId)
+        val proof = ops.internalFindVerifiedTransaction(stateAndRef.ref.txhash)!!.toProof()
+        val reply = QueryOuterClass.QueryPacketAcknowledgementResponse.newBuilder()
+                .setAcknowledgement(stateAndRef.state.data.acknowledgements[request.sequence]!!.toByteString())
+                .setProof(proof.toByteString())
+                .setProofHeight(Client.Height.getDefaultInstance())
+                .build()
+        responseObserver.onNext(reply)
+        responseObserver.onCompleted()
+    }
 }
