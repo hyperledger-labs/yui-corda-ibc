e1b6be37fcb8efe5e304ff68ce4c93ed47503839
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-02-05 11:27:05 +0900

    remove old unnecessary module 'cosmos-experiment'

--- cosmos-experiment/src/main/kotlin/jp/datachain/corda/ibc/cosmos/ReqPath.kt
+++
@@ -1,6 +0,0 @@
-package jp.datachain.corda.ibc.cosmos
-
-@Target(AnnotationTarget.CLASS)
-annotation class ReqPath(val pathFormat: String)
-
-fun ReqPath.path(vararg args: String) = pathFormat.format(*args)
--- cosmos-experiment/src/main/kotlin/jp/datachain/cosmos/x/auth/client/rest/BroadcastReq.kt
+++
@@ -1,8 +0,0 @@
-package jp.datachain.cosmos.x.auth.client.rest
-
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
-import com.fasterxml.jackson.databind.annotation.JsonNaming
-import jp.datachain.cosmos.x.auth.types.StdTx
-
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
-data class BroadcastReq(val tx: StdTx, val mode: String)
