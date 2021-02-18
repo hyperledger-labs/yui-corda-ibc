328c2c047f674567c1260d0910b7ab886b20536b
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-02-19 01:24:46 +0900

    use JSON in order to encode Acknowledgement
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- contracts/src/main/kotlin/jp/datachain/corda/ibc/ics20/json-conversion.kt
+++ contracts/src/main/kotlin/jp/datachain/corda/ibc/ics20/json-conversion.kt
@@ -1,11 +1,17 @@
 package jp.datachain.corda.ibc.ics20
 
 import com.google.protobuf.ByteString
+import com.google.protobuf.MessageOrBuilder
 import com.google.protobuf.util.JsonFormat
 import ibc.applications.transfer.v1.Transfer
+import ibc.core.channel.v1.ChannelOuterClass
 
-fun Transfer.FungibleTokenPacketData.toJson() = ByteString.copyFromUtf8(JsonFormat.printer().print(this))!!
+fun MessageOrBuilder.toJson() = ByteString.copyFromUtf8(JsonFormat.printer().print(this))!!
 
 fun ByteString.toFungibleTokenPacketData() = Transfer.FungibleTokenPacketData.newBuilder().also{
+    JsonFormat.parser().merge(toStringUtf8(), it)
+}.build()!!
+
+fun ByteString.toAcknowledgement() = ChannelOuterClass.Acknowledgement.newBuilder().also{
     JsonFormat.parser().merge(toStringUtf8(), it)
 }.build()!!
--- contracts/src/main/kotlin/jp/datachain/corda/ibc/ics26/HandlePacketAcknowledgement.kt
+++ contracts/src/main/kotlin/jp/datachain/corda/ibc/ics26/HandlePacketAcknowledgement.kt
@@ -1,7 +1,7 @@
 package jp.datachain.corda.ibc.ics26
 
-import ibc.core.channel.v1.ChannelOuterClass
 import ibc.core.channel.v1.Tx
+import jp.datachain.corda.ibc.ics20.toAcknowledgement
 import jp.datachain.corda.ibc.ics23.CommitmentProof
 import jp.datachain.corda.ibc.ics24.Identifier
 import jp.datachain.corda.ibc.ics25.Handler
@@ -10,7 +10,7 @@ import java.security.PublicKey
 data class HandlePacketAcknowledgement(val msg: Tx.MsgAcknowledgement): DatagramHandler {
     override fun execute(ctx: Context, signers: Collection<PublicKey>) {
         val module = ModuleCallbacks.lookupModule(Identifier(msg.packet.destinationPort))
-        val ack = ChannelOuterClass.Acknowledgement.parseFrom(msg.acknowledgement)!!
+        val ack = msg.acknowledgement.toAcknowledgement()
         module.onAcknowledgePacket(ctx, msg.packet, ack)
         Handler.acknowledgePacket(
                 ctx,
