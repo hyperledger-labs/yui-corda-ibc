66a53d9ba4f5f1745fad11d4746e12b03c9471eb
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-03-29 21:16:40 +0900

    rust: add query-{client,consensus}-state subcommands
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- rust/src/client.rs
+++ rust/src/client.rs
@@ -5,14 +5,46 @@ use super::Result;
 use ibc::core::client::v1 as ibc_client;
 use ibc::lightclients::corda::v1 as ibc_corda;
 
+use prost_types::Any;
+
+async fn connect_tx(
+    endpoint: &str,
+) -> Result<ibc_client::msg_client::MsgClient<tonic::transport::Channel>> {
+    let client = ibc_client::msg_client::MsgClient::connect(endpoint.to_owned()).await?;
+    Ok(client)
+}
+
+async fn connect_query(
+    endpoint: &str,
+) -> Result<ibc_client::query_client::QueryClient<tonic::transport::Channel>> {
+    let client = ibc_client::query_client::QueryClient::connect(endpoint.to_owned()).await?;
+    Ok(client)
+}
+
+pub async fn create_client(
+    endpoint: &str,
+    client_id: &str,
+    client_state: Any,
+    consensus_state: Any,
+) -> Result<()> {
+    let mut client = connect_tx(endpoint).await?;
+    client
+        .create_client(ibc_client::MsgCreateClient {
+            client_id: client_id.to_owned(),
+            client_state: Some(client_state),
+            consensus_state: Some(consensus_state),
+            signer: String::default(),
+        })
+        .await?;
+    Ok(())
+}
+
 pub async fn create_corda_client(
     endpoint: &str,
     client_id: &str,
     counterparty_base_hash: &str,
     counterparty_notary_key: &str,
 ) -> Result<()> {
-    let mut client = ibc_client::msg_client::MsgClient::connect(endpoint.to_owned()).await?;
-
     let client_state = util::pack_any(
         "/ibc.lightclients.corda.v1.ClientState",
         &ibc_corda::ClientState {
@@ -28,13 +60,37 @@ pub async fn create_corda_client(
         },
     )?;
 
-    client
-        .create_client(ibc_client::MsgCreateClient {
+    create_client(endpoint, client_id, client_state, consensus_state).await
+}
+
+pub async fn query_client_state(
+    endpoint: &str,
+    client_id: &str,
+) -> Result<ibc_client::QueryClientStateResponse> {
+    let mut client = connect_query(endpoint).await?;
+    let response = client
+        .client_state(ibc_client::QueryClientStateRequest {
             client_id: client_id.to_owned(),
-            client_state: Some(client_state),
-            consensus_state: Some(consensus_state),
-            signer: String::default(),
         })
         .await?;
-    Ok(())
+    Ok(response.into_inner())
+}
+
+pub async fn query_consensus_state(
+    endpoint: &str,
+    client_id: &str,
+    version_number: u64,
+    version_height: u64,
+    latest_height: bool,
+) -> Result<ibc_client::QueryConsensusStateResponse> {
+    let mut client = connect_query(endpoint).await?;
+    let response = client
+        .consensus_state(ibc_client::QueryConsensusStateRequest {
+            client_id: client_id.to_owned(),
+            version_number,
+            version_height,
+            latest_height,
+        })
+        .await?;
+    Ok(response.into_inner())
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -61,6 +61,29 @@ enum Opt {
         #[structopt(short = "n", long)]
         counterparty_notary_key: String,
     },
+    QueryClientState {
+        #[structopt(short, long, default_value = "http://localhost:9999")]
+        endpoint: String,
+
+        #[structopt(short, long)]
+        client_id: String,
+    },
+    QueryConsensusState {
+        #[structopt(short, long, default_value = "http://localhost:9999")]
+        endpoint: String,
+
+        #[structopt(short, long)]
+        client_id: String,
+
+        #[structopt(short = "n", long)]
+        version_number: u64,
+
+        #[structopt(short = "h", long)]
+        version_height: u64,
+
+        #[structopt(short, long)]
+        latest_height: bool,
+    },
 }
 
 #[tokio::main]
@@ -101,6 +124,30 @@ async fn main() -> Result<()> {
                 &counterparty_notary_key,
             )
             .await?;
+        }
+        Opt::QueryClientState {
+            endpoint,
+            client_id,
+        } => {
+            let response = client::query_client_state(&endpoint, &client_id).await?;
+            println!("{:?}", response);
+        }
+        Opt::QueryConsensusState {
+            endpoint,
+            client_id,
+            version_number,
+            version_height,
+            latest_height,
+        } => {
+            let response = client::query_consensus_state(
+                &endpoint,
+                &client_id,
+                version_number,
+                version_height,
+                latest_height,
+            )
+            .await?;
+            println!("{:?}", response);
         }
     }
 
