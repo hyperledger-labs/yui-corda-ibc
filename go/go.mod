f93f3b20f867c344101e3210e2f9099c67b57bde
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2021-04-02 16:55:56 +0900

    go: add AppModuleBasic for corda-ibc
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- go/go.mod
+++ go/go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/confio/ics23/go v0.6.3
 	github.com/cosmos/cosmos-sdk v0.40.0-rc3
 	github.com/gogo/protobuf v1.3.1
+	github.com/gorilla/mux v1.8.0
+	github.com/grpc-ecosystem/grpc-gateway v1.15.2
+	github.com/spf13/cobra v1.1.1
 	google.golang.org/grpc v1.33.0
 	google.golang.org/protobuf v1.25.0
 )
---
+++ go/x/ibc/light-clients/xx-corda/module.go
@@ -0,0 +1,54 @@
+package corda
+
+import (
+	"encoding/json"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/datachainlab/corda-ibc/go/x/ibc/light-clients/xx-corda/types"
+	"github.com/gorilla/mux"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/spf13/cobra"
+)
+
+// ----------------------------------------------------------------------------
+// AppModuleBasic
+// ----------------------------------------------------------------------------
+
+// AppModuleBasic implements the AppModuleBasic interface for the corda-ibc module.
+type AppModuleBasic struct{}
+
+// Name returns the corda-ibc module's name.
+func (AppModuleBasic) Name() string {
+	return types.ModuleName
+}
+
+func (AppModuleBasic) RegisterLegacyAminoCodec(*codec.LegacyAmino) {}
+
+// RegisterInterfaces registers module concrete types into protobuf Any.
+func (AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	types.RegisterInterfaces(registry)
+}
+
+func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {}
+
+func (AppModuleBasic) DefaultGenesis(cdc codec.JSONMarshaler) json.RawMessage {
+	return nil
+}
+
+func (AppModuleBasic) ValidateGenesis(cdc codec.JSONMarshaler, config client.TxEncodingConfig, bz json.RawMessage) error {
+	return nil
+}
+
+func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Router) {}
+
+func (a AppModuleBasic) RegisterGRPCRoutes(_ client.Context, _ *runtime.ServeMux) {}
+
+func (a AppModuleBasic) GetTxCmd() *cobra.Command {
+	return nil
+}
+
+func (AppModuleBasic) GetQueryCmd() *cobra.Command {
+	return nil
+}
