abc75cdc94c627d96330eb868caae2c82c799849
Masanori Yoshida <masanori.yoshida@datachain.jp>
2022-10-07 14:11:57 +0900

    turn off a trick for obtaining test data for Corda ELC
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- grpc-adapter/src/main/kotlin/jp/datachain/corda/ibc/grpc_adapter/Client.kt
+++ grpc-adapter/src/main/kotlin/jp/datachain/corda/ibc/grpc_adapter/Client.kt
@@ -370,7 +370,6 @@ object Client {
             assert(it.sequencesList.containsAll(listOf(1L, 2L, 3L)))
         }
 
-        /*
         // recv acks for $10, $20 and $30 @ A
         repeat(3) {
             val seq = (it + 1).toLong()
@@ -400,7 +399,6 @@ object Client {
         }.build()).let {
             assert(it.sequencesCount == 0)
         }
-        */
 
         // check acks
         channelQueryServiceB.packetAcknowledgements(QueryPacketAcknowledgementsRequest.newBuilder().apply{
